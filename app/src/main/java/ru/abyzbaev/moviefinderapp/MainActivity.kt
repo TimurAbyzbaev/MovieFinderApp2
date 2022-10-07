d10fa8d8048ae50fe381db30d5dbe886772310bf
Timur <timur@vlaga.pro>
2022-10-07 15:25:53 +0500

    Initial commit

---
+++ app/src/main/java/ru/abyzbaev/moviefinderapp/MainActivity.kt
@@ -0,0 +1,23 @@
+package ru.abyzbaev.moviefinderapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import android.widget.Toast.LENGTH_SHORT
+import ru.abyzbaev.moviefinderapp.R
+import java.time.Duration
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+
+    }
+}
+
