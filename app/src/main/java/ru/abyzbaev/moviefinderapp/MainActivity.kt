8f6bd4b347ef100248659546cb570bb39073ce68
Timur <timur@vlaga.pro>
2022-10-07 15:31:06 +0500

    Урок 1 начало Добавил дата класс, проинициализировал кнопку и textView

--- app/src/main/java/ru/abyzbaev/moviefinderapp/MainActivity.kt
+++ app/src/main/java/ru/abyzbaev/moviefinderapp/MainActivity.kt
@@ -16,8 +16,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        
+        val myTestClass1 = MyDataClass();
+        val myTestClass2 = MyDataClass("MyVal1", "MyVal2")
+
+        val button: Button = findViewById(R.id.btn)
+        val title: TextView = findViewById(R.id.title)
+        val textView1: TextView = findViewById(R.id.t2)
+        val textView2: TextView = findViewById(R.id.t3)
+
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Toast.makeText(baseContext, "Toast!", LENGTH_SHORT).show()
+            }
+        })
+
 
     }
 }
 
+data class MyDataClass(val val1: String = "val1", val val2: String = "val2")
